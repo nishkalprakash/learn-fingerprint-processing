87fde32ed0331857c49e28fa010841f79b8bd361
Nishkal Prakash <nishkalprakash@gmail.com>
2023-06-05 21:19:29 +0530

    work in progress - class for image

--- classes.py
+++ classes.py
@@ -1,8 +1,15 @@
+#%% Imports
 from matplotlib import pyplot as plt
 import cv2 as cv
 from pathlib import Path
 import numpy as np
+from pprint import pprint as pp
+#%% Logging
+from setup_logger import get_logger
 
+logger = get_logger(__name__)
+
+#%% Image Class  
 class Image:
     """Returns an object of an Image 
         if width is integer and height not provided:
@@ -22,28 +29,116 @@ class Image:
         Args:
             width (int|str|path|iterable): width of the image
             height (int): height of the image
-        """
-        
+        """  
+        if isinstance(width,str|Path):
+            """if width is a string or path then load the image from the given path"""
+            logger.info(f"Loading image from path: {width}")
+            self.init_from_path(width)
+            return
 
-        # if width is a string or Path then load the image from the given path
-        if isinstance(width,str) or isinstance(width,Path):
-            self.load(width)
-            return
-        # if width is an iterable then load the image from the given iterable
         if hasattr(width,"__iter__"):
-            self.image = np.array(width)
-            self.height,self.width = self.image.shape[:2]
+            """if width is an iterable then load the image from the given iterable"""
+            logger.info(f"Loading image from iterable")
+            self.init_from_list(width)
             return
-        # if width is not given, then skip the initialization
+
+        if isinstance(width,list|tuple|np.ndarray):
+            """if width is an list|tuple|nd.ndarray then load the image from the given iterable"""
+            logger.info(f"Loading image from {type(width)}")
+            self.init_from_list(width)
+            return
+
+        
         if width == -1:
+            """if width is not given, then skip the initialization"""
+            logger.info(f"Skipping initialization of image")
             self.image=None
             return
-        # if height is not given, then height is equal to width
+        
         self.width = width
+        
+        # if height is not given, then height is equal to width
         self.height = width if height == -1 else height
         # image stores the image as a numpy array with 0 as default values
+        logger.info(f"Initializing empty 1 channel image with 0 of size {self.width}x{self.height}")
         self.image = np.array([[0]*self.width]*self.height)
         
+    
+    ## START INIT METHODS for image ##
+    def __getitem__(self,key):
+        """Returns the value of the image at the given key"""
+        d = self.__dict__
+        if key in d.keys():
+            return d[key]
+        logger.warning(f"Unknown key: {key}")
+        # return self.__dict__[key]
+    def init_from_dict(self, doc):
+        """Initializes the image from the given mongo document"""
+        ## copy all the fields from the document to __dict__
+        logger.info(f"Initializing image from mongo document")
+        for key in doc.keys():
+            self.__dict__[key] = doc[key]
+            
+        # for key in doc.keys():
+            # match key:
+            #     case "image":
+            #         self.image = np.array(doc["image"])
+            #     case "path":
+            #         self.path = Path(doc["path"])
+            #     case "mv":
+            #         self.mv = doc["mv"]
+            #     case "height":
+            #         self.height = doc["height"]
+            #     case "width":
+            #         self.width = doc["width"]
+            #     case "core":
+            #         self.core = doc["core"]
+            #     case "db":
+            #     case _:
+            #         logger.warning(f"Unknown key: {key}")
+        # else:
+        #     self.image = np.array()
+        #     self.path = Path(doc["path"])
+        #     self.init_height_width()
+    
+    def init_one_from_mongo_coll(self,coll,query={}):
+        """Initializes the image from the given mongo collection"""
+        ## Count the number of images that match the query and only init if count is 1
+        if coll.count_documents(query) == 0:
+            logger.error(f"There are no images that match the query: {query}")
+            raise Exception("There are no images that match the query")
+        elif coll.count_documents(query) > 1:
+            logger.error(f"There are more than one images that match the query: {query}")
+            raise Exception("There are more than one images that match the query")
+        else:
+            # populate all fields from the collection
+            logger.info(f"Initializing image from mongo collection with query: {query}")
+            self.init_from_dict(coll.find_one(query))
+
+            # pass
+
+
+        # self.image = np.array(coll.find_one())
+        # self.init_height_width()
+
+
+    def init_from_list(self,raw_data):
+        """Initializes the image from the given list"""
+        self.image = np.array(raw_data)
+        self.init_height_width()
+
+    def init_from_path(self,path):
+        """Loads the image from the given path"""
+        self.image = cv.imread(str(path))
+        self.init_height_width()
+        self.path=Path(path)
+
+    def init_height_width(self):
+        """Initializes the height and width of the image"""
+        self.height,self.width = self.image.shape[:2]
+    ## END INIT METHODS for image ##
+
+
     def plot(self,title="",**kwargs):
         """Plots the image using matplotlib"""
         plt.axis('off')
@@ -51,19 +146,35 @@ class Image:
         plt.imshow(self.image,**kwargs)
         # plt.show()
     
+
     def show(self,title=""):
         """Shows the image using opencv"""
         cv.imshow(title,self.image)
         cv.waitKey(0)
         cv.destroyAllWindows()
         
-    def load(self,path):
-        """Loads the image from the given path"""
-        self.image = cv.imread(str(path))
-        self.height,self.width = self.image.shape[:2]
-        self.path=Path(path)
-
+    
     def save(self,path):
         """Saves the image to the given path"""
         cv.imwrite(path,self.image)
         # plt.savefig(f"{path}.png", bbox_inches='tight')
+
+    def get_minutiae(self, method=None):
+        """Returns the extract the minutiae from the fingerprint image extracted using the given method"""
+        self.avail_minutiae_methods([
+            "crossing_number",
+            "minutiae_net",
+        ])
+        self.default_minutiae_method = "crossing_number"
+        if method is None:
+            logger.info(f"Available minutiae extraction methods:\n{pp(self.minutiae_methods)}")
+            logger.info(f"returning minutiae extrated using default method [{self.default_minutiae_method}]")
+            return self.extract_minutiae(self.default_minutiae_method)
+        elif method in self.minutiae_methods:
+            logger.info(f"returning minutiae extrated using method [{method}]")
+            return self.extract_minutiae(method)
+        return self.minutiae
+
+    def extract_minutiae(self, method):
+        """Extract the minutiae from the fingerprint image using the given method"""
+        self.minutiae = 
